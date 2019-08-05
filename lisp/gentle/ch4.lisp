8e2de9a02b4737ca6bc0d4f1a18765b6e953a996
Jc <johnsjc@gmail.com>
2019-08-05 13:53:11 -0600

    update ch4.lisp

--- lisp/gentle/ch4.lisp
+++ lisp/gentle/ch4.lisp
@@ -1,29 +1,29 @@
-(defun make-even (n)
+(defun 4-1 (n)
   (if (evenp n)
       n
       (+ 1 n)))
 
-(defun further (n)
+(defun 4-2 (n)
   (if (< n 0)
       (- n 1)
       (+ n 1)))
 
-(defun my-not (value)
+(defun 4-3 (value)
   (if value
       nil
       t))
 
-(defun ordered (x y)
+(defun 4-4 (x y)
   (if (< x y)
       (list x y)
       (list y x)))
 
-(defun my-abs (n)
+(defun 4-6 (n)
   (if (< n 0)
       (- n)
       n))
 
-(defun emphasize3 (phrase)
+(defun 4-8 (phrase)
   (cond ((equal (first phrase) 'good)
          (cons 'great (rest phrase)))
         ((equal (first phrase) 'bad)
@@ -31,12 +31,12 @@
         (t
          (cons 'very phrase))))
 
-(defun make-odd (x)
+(defun 4-9 (x)
   (cond ((not (oddp x))
          (+ x 1))
         (t x)))
 
-(defun constrain-cond (x min max)
+(defun 4-10-cond (x min max)
   (cond ((< x min)
          min)
         ((> x max)
@@ -44,14 +44,14 @@
         (t
          x)))
 
-(defun constrain-if (x min max)
+(defun 4-10-if (x min max)
   (if (< x min)
       min
       (if (> x max)
           max
           x)))
 
-(defun firstzero (nums)
+(defun 4-11 (nums)
   (cond ((zerop (first nums))
          'first)
         ((zerop (second nums))
@@ -61,13 +61,13 @@
         (t
          'none)))
 
-(defun cycle (n)
+(defun 4-12 (n)
   (cond ((equal 99 n)
          1)
         (t
          (+ n 1))))
 
-(defun how-compute (op1 op2 result)
+(defun 4-13 (op1 op2 result)
   (cond ((equal result (+ op1 op2))
          'sum-of)
         ((equal result (* op1 op2))
@@ -75,3 +75,107 @@
         (t
          'beats-me)
 ))
+
+(defun 4-15 (x y)
+  (or (equal x y)
+      (> x y)))
+
+(defun 4-16 (n)
+  (cond ((and (oddp n) (> n 0))
+         (* n n))
+        ((and (oddp n) (< n 0))
+         (* n 2))
+        (t (/ n 2))))
+
+(defun 4-17 (x y)
+  (or
+   (and
+    (or (equal x 'boy) (equal x 'girl))
+    (equal y 'child))
+   (and
+    (or (equal x 'man) (equal x 'woman))
+    (equal y 'adult))
+   ))
+
+(defun 4-18 (player1 player2)
+  (cond ((equal player1 player2)
+         'tie)
+        ((or
+          (and
+           (equal player1 'rock)
+           (equal player2 'scissors))
+          (and
+           (equal player1 'paper)
+           (equal player2 'rock))
+          (and
+           (equal player1 'scissors)
+           (equal player2 'paper)))
+         'first-wins)
+        (t
+         'second-wins)))
+
+(defun 4-19-cond (x y z w)
+  (cond ((not x)
+         nil)
+        ((not y)
+         nil)
+        ((not z)
+         nil)
+        (t
+         w)))
+
+(defun 4-19-if (x y z w)
+  (if x
+      (if y
+          (if z
+              w))))
+
+(defun 4-20-if (x y)
+  (if (equal x y)
+      'equal
+      (if (< x y)
+          'first-smaller
+          'first-bigger)))
+
+(defun 4-20-and-or (x y)
+  (or
+   (and (equal x y)
+        'equal)
+   (and (< x y)
+        'first-smaller)
+   'first-bigger))
+
+(defun 4-21-if (x y)
+  (if (> x y)
+      t
+      (if (zerop x)
+          t
+          (zerop y))))
+
+(defun 4-21-cond (x y)
+  (cond ((> x y)
+         t)
+        ((zerop x)
+         t)
+        (t (zerop y))))
+
+(defun 4-22-cond (temp scale)
+  (cond ((equal scale 'fahrenheit)
+         (> temp 212))
+        ((equal scale 'celcius)
+         (> temp 100))))
+
+(defun 4-22-if (temp scale)
+  (if (equal scale 'fahrenheit)
+      (> temp 212)
+      (if (equal scale 'celcius)
+          (> temp 100))))
+
+(defun 4-22-and-or (temp scale)
+  (or
+   (and
+    (equal scale 'fahrenheit)
+    (> temp 212))
+   (and
+    (equal scale 'celcius)
+    (> temp 100))))
